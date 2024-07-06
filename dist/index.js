6d827900bf863a88cbd07df75372ad394b94d0b9
unknown <sontranviet21@gmail.com>
2024-07-06 17:41:52 +0700

    add

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greet = void 0;
+var greet = function (name) {
+    var res = "Hello, ".concat(name, "!");
+    console.log(res);
+    return res;
+};
+exports.greet = greet;
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export const greet = (name: string): string => {
+    let res:string = `Hello, ${name}!`;
+    console.log(res)
+    return res;
+};
+  
